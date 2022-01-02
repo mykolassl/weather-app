a22ade1720c7ec2bbe28789b66460a0e38a99600
mykolassl <emykolas@gmail.com>
2022-01-02 14:55:11 +0200

    Added fetching of weather data

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,67 @@
+const options = { 
+    weekday: 'long',
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric' 
+};
+
+const cityInput = document.getElementById('city-input');
+const submitBtn = document.getElementById('search-button');
+
 let localTime = document.getElementById("local-time");
 let localDate = document.getElementById("local-date");
 
 let cityName = document.getElementById("city-name");
 let countryName = document.getElementById("country-name");
 
-const cityInput = document.getElementById('city-input');
-const submitBtn = document.getElementById('search-button');
+let regionNames = new Intl.DisplayNames(['en'], { type: 'region' });
 
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+// Set time and date based on utc offset
+
+function setTime(offset) {
+    const date = new Date();
+    const offsetDate = new Date(date.getTime() + (offset * 1000));
+
+    localTime.innerText = `${offsetDate.getUTCHours()}:${offsetDate.getUTCMinutes() < 10 ? '0' + offsetDate.getUTCMinutes() : offsetDate.getUTCMinutes()}`;
+    localDate.innerText = offsetDate.toLocaleDateString(undefined, options);
+}
+
+async function getCoordinates(city) {
+    console.log('Fetching data..')
+    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${token}`);
+    return res.json();
+}
+
+async function getWeatherData(lat, lon) {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${token}`);
+    return res.json();
+}
+
+// Set the default position and time
+
+cityName.innerText = 'Vilnius';
+countryName.innerText = 'Lithuania';
+setTime(7200)
 
 // Form submission
 
-submitBtn.addEventListener('click', (e) => {
+submitBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    console.log(cityInput.value);
+    
+    if(cityInput.value == '') return;
+
+    try {
+        let [{ lat, lon, country, name }] = await getCoordinates(cityInput.value);
+        cityName.innerText = name;
+        countryName.innerText = regionNames.of(country);
+
+        const data = await getWeatherData(lat, lon);
+        setTime(data.timezone_offset);
+
+        cityInput.value = ''; 
+    } catch {
+        return alert('City not found');
+    }
+
 })
 
-// Placeholder code
-
-cityName.innerText = 'Vilnius';
-countryName.innerText = "Lithuania";
-
-setInterval(() => {
-    const date = new Date();
-
-    localTime.innerText = `${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`;
-    localDate.innerText = date.toLocaleDateString(undefined, options);
-}, 800)
